28a8a84d882e9c83cc64f8539d9c42c289639852
DonPabla <113447403+DonPabla@users.noreply.github.com>
2023-03-13 15:57:33 +0600

    Add files via upload

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,17 @@ import heart from '../assets/heart.png'
 import lupa from '../assets/lupa.png'
 import cart from '../assets/cart_img.png'
 import vector from '../assets/vector.png'
+import { useState } from 'react'
 
 const Header = (Props) => {
+    const [burger, setBurger] = useState(false)
+
+    function burgerMenu(e) {
+        setBurger(!burger)
+    }
+
+    console.log(burger)
+
     return (
         <header>
             <div className={module.header_a}>
@@ -16,10 +25,21 @@ const Header = (Props) => {
                     <a href='#'>СВЯЗАТЬСЯ С НАМИ</a>
                     <a href='#'>АКЦИИ</a>
                 </div>
-                <div className={module.burger}>
-                <i class="fa-solid fa-bars"></i>
+                {burger && (<div className={module.container_burger}>
+                    <a href='#'>ГЛАВНЫЙ</a>
+                    <a href='#'>КАТАЛОГ</a>
+                    <a href='#'>СВЯЗАТЬСЯ С НАМИ</a>
+                    <a href='#'>АКЦИИ</a>
+                </div>)}
+                {burger && (<div className={module.container_burger}>
+                    <a href='#'>ГЛАВНЫЙ</a>
+                    <a href='#'>КАТАЛОГ</a>
+                    <a href='#'>СВЯЗАТЬСЯ С НАМИ</a>
+                    <a href='#'>АКЦИИ</a>
+                </div>)}
+                <div className={module.burger} onClick={burgerMenu}>
+                    <i class="fa-solid fa-bars"></i>
                 </div>
-
                 <div className={module.head_pic}>
                     <a href='#'><img src={vector}></img></a>
                     <a href='#'><img src={cart}></img></a>
